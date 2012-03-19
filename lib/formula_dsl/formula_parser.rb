b1f70851d4a6d17a13de96d9077f1d81d17638c1
Sergio Junior <sergior.jr@gmail.com>
2012-03-19 17:53:40 -0300

    first version of parser

--- lib/formula_dsl/formula_parser.rb
+++ lib/formula_dsl/formula_parser.rb
@@ -1,4 +1,26 @@
 #encoding: utf-8
 class FormulaParser < Parslet::Parser
 
+  rule :space do
+    match('\s').repeat(1)
+  end
+
+  rule :space? do
+    space.maybe
+  end
+
+  rule :number do
+    match('\d').repeat(1)
+  end
+
+  rule :operator do
+    match( ['+'] )
+  end
+
+  rule :expression do
+    number >> space? >> operator >> space? >> number
+  end
+
+  root :expression
+
 end
--- spec/formula_dsl/formula_parser_spec.rb
+++ spec/formula_dsl/formula_parser_spec.rb
@@ -3,10 +3,21 @@ require 'spec_helper'
 
 describe FormulaParser do
 
-  it "should recognize '1+1' as a valid expression"
+  let(:parser){ subject }
 
-  it "should raise error a incomplet expression like '1 +'"
+  context "For binary operations like ( +, - , *, / )" do
 
-  it "should recogize a valid expression with blank spaces like 1 + 2"
+    it "should recognize '1+1' as a valid expression" do
+      lambda{ parser.parse("1+1") }.should_not raise_error
+    end
+
+    it "should raise error a incomplet expression like '1 +'" do
+      lambda{ parser.parse("1 +") }.should raise_error(Parslet::ParseFailed)
+    end
+
+    it "should recogize a valid expression with blank spaces like 1 + 2" do
+      lambda{ parser.parse("1 + 2") }.should_not raise_error
+    end
+  end
 
 end
