ce5b43bdcba6a0c5012747444114200f9250a071
Sergio Junior <sergior.jr@gmail.com>
2012-03-21 13:38:43 -0300

    simple_grammar first version

--- lib/formula_dsl/formula_parser.rb
+++ lib/formula_dsl/formula_parser.rb
@@ -14,35 +14,58 @@ class FormulaParser < Parslet::Parser
   rule(:mult_operator) { match(['*']) }
   rule(:div_operator ) { match(['/']) }
 
-  rule :multiplication do
-    (number.as(:left) >> mult_operator >> (multiplication | number).as(:right)).as(:*)
-  end
-
+  #binary operations + - / *
   rule :addition do
-    (number.as(:left) >> add_operator >> ( single_operations ).as(:right)).as(:+)
+    ( number.as(:left) >> space? >> add_operator >> space? >> ( number ).as(:right) ).as(:+)
   end
 
-  rule :multiplication_expression do
-    ( ( single_operations ).as(:left) >> add_operator >> (addition | number).as(:right) ).as(:+)
+  rule :subtraction do
+    ( number.as(:left) >> space? >> sub_operator >> space? >> ( number ).as(:right) ).as(:-)
   end
 
-  rule :addition_expression do
-    ( addition >> mult_operator >> (multiplication | number).as(:right) )
+  rule :multiplication do
+    ( number.as(:left) >> space? >> mult_operator >> space? >> ( number ).as(:right) ).as(:*)
+  end
+
+  rule :division do
+    ( number.as(:left) >> space? >> div_operator >> space? >> ( number ).as(:right) ).as(:/)
   end
 
   rule :expression do
-     multiplication_expression | addition_expression
+    addition | subtraction | multiplication | division
   end
 
-  rule :single_operations do
-    addition | multiplication | number
-  end
+  root :expression
 
-  rule :list do
-    expression | single_operations
-  end
+  # rule :addition do
+  #   (number.as(:left) >> space? >> add_operator >> >> space? >> ( single_operations ).as(:right)).as(:+)
+  # end
 
-  root :list
+  # rule :multiplication do
+  #   (number.as(:left) >> mult_operator >> (multiplication | number).as(:right)).as(:*)
+  # end
+
+  # rule :multiplication_expression do
+  #   ( ( single_operations ).as(:left) >> add_operator >> (addition | number).as(:right) ).as(:+)
+  # end
+
+  # rule :addition_expression do
+  #   ( addition >> mult_operator >> (multiplication | number).as(:right) )
+  # end
+
+  # rule :expression do
+  #    multiplication_expression | addition_expression
+  # end
+
+  # rule :single_operations do
+  #   addition | multiplication | number
+  # end
+
+  # rule :list do
+  #   expression | single_operations
+  # end
+
+  # root :list
 end
 
 #  expression = left_side operator rigth_side
--- spec/formula_dsl/formula_parser_spec.rb
+++ spec/formula_dsl/formula_parser_spec.rb
@@ -5,124 +5,33 @@ describe FormulaParser do
 
   let(:parser){ subject }
 
-  context "For simple expressions (without functions)" do
-    # it "should recognize multiply operations" do
-    #   ast = parser.parse('2*5')
-    #   ast[:*][:left].to_i.should == 2
-    #   ast[:*][:right].to_i.should == 5
-    # end
-    # it "should recognize division operations" do
-    #   ast = parser.parse('2/5')
-    #   ast[:/][:left].to_i.should == 2
-    #   ast[:/][:right].to_i.should == 5
-    # end
-  #   it "should recognize sum operations" do
-  #     ast = parser.parse('2+5')
-  #     ast[:+][:left].to_i.should == 2
-  #     ast[:+][:right].to_i.should == 5
-  #   end
-  end
+    it "should recognize expression '2 + 1' " do
+      ast = parser.parse('2 + 1')
+      ast.to_s.should == %Q({:+=>{:left=>"2"@0, :right=>"1"@4}})
+    end
 
-  context "For more complex expressions (without functions)" do
-    # it "should recognize 2+5+10" do
-    #   ast = parser.parse('2+5+10')
-    #   ast[:+][:left].to_i.should == 2
-    #   ast[:+][:right].should be_a Hash
-    #   ast[:+][:right][:+].should_not nil
-    #   ast[:+][:right][:+][:left].to_i.should == 5
-    #   ast[:+][:right][:+][:right].to_i.should == 10
-    # end
+    it "should recognize expression '2 - 1" do
+      ast = parser.parse('2 - 1')
+      ast.to_s.should == %Q({:-=>{:left=>"2"@0, :right=>"1"@4}})
+    end
 
-    # it "should recognize 2*5*10" do
-    #   ast = parser.parse('2*5*10')
-    #   ast[:*][:left].to_i.should == 2
-    #   ast[:*][:right].should be_a Hash
-    #   ast[:*][:right][:*].should_not nil
-    #   ast[:*][:right][:*][:left].to_i.should == 5
-    #   ast[:*][:right][:*][:right].to_i.should == 10
-    # end
+    it "should recognize expression '2 * 1" do
+      ast = parser.parse('2 * 1')
+      ast.to_s.should == %Q({:*=>{:left=>"2"@0, :right=>"1"@4}})
+    end
 
-    it "should recognize 2*5+10" do
-      # ast = parser.parse('2-5')
-      # puts ast.inspect
-
-      ast = parser.parse('2+5')
-      puts ast.inspect
-      ast = parser.parse('2+5+10')
-      puts ast.inspect
-      ast = parser.parse('2*5')
-      puts ast.inspect
-      ast = parser.parse('2*5+10')
-      puts ast.inspect
-      ast = parser.parse('2*5+10+8')
-      puts ast.inspect
-      ast = parser.parse('2*5*2+10+8')
-      puts ast.inspect
-      ast = parser.parse('2+4*10')
-      puts ast.inspect
-      ast = parser.parse('2*5*5')
-      puts ast.inspect
-      ast = parser.parse('1+3+4+1+30*50*2+10+4+5')
-      puts ast
-      ast = parser.parse('2+5+3+4*4')
-      puts ast.inspect
-
-
-      # ast[:+][:left].should be_a Hash
-      # ast[:+][:right].to_i should == 10
-      # ast[:+][:right][:*].should_not nil
-      # ast[:+][:right][:*][:left].to_i.should == 2
-      # ast[:+][:right][:*][:right].to_i.should == 5
+    it "should recognize expression '2 / 2" do
+      ast = parser.parse('2 / 2')
+      ast.to_s.should == %Q({:/=>{:left=>"2"@0, :right=>"2"@4}})
     end
 
 
-  end
-
-    # it "should recognize subtract operations" do
-    #   ast = parser.parse('2-5')
-    #   ast[:-][:left].to_i.should == 2
-    #   ast[:-][:right].to_i.should == 5
+    # it "should recognize a function like 'Month(data)' " do
+    #   ast = parser.parse('Month(data)')
     # end
 
-    # it "should recognize a valid expression with blank spaces like '  4   +   3  '" do
-    #   lambda{ parser.parse("  4   +  3  ") }.should_not raise_error
+    # it "should recognize the expression 'Month(data) - 1' " do
+    #   ast = parser.parse('Month(data) - 1')
     # end
-
-    # it "should raise error a incomplet expression like '1 +'" do
-    #   lambda{ parser.parse("1 +") }.should raise_error(Parslet::ParseFailed)
-    # end
-
-  # context "For complex expression like '2 + 5 - 3' (sill without functions)" do
-  #   it "should recognize '2 + 5 - 3' as a valid expression" do
-  #     ast = parser.parse("2 + 5 -3")
-  #     ast[:left].to_i.should == 2
-  #     ast[:operator].to_s.should == '+'
-  #     ast[:right].should be_a Hash
-  #     ast[:right][:left].to_i.should == 5
-  #     ast[:right][:operator].should == '-'
-  #     ast[:right][:right].to_i.should == 3
-  #   end
-
-  #   it "should recognize '12 + 15 - 3 * 4 / 2' as a valid expression" do
-  #     ast = parser.parse("12 + 15 -3 * 4/2")
-  #     ast[:left].to_i.should == 12
-  #     ast[:operator].to_s.should == '+'
-  #     ast[:right].should be_a Hash
-  #     ast[:right][:left].to_i.should == 15
-  #     ast[:right][:operator].should == '-'
-  #     ast[:right][:right].should be_a Hash
-  #     ast[:right][:right][:left].to_i.should == 3
-  #     ast[:right][:right][:operator].should == '*'
-  #     ast[:right][:right][:right].should be_a Hash
-  #     ast[:right][:right][:right][:left].to_i.should == 4
-  #     ast[:right][:right][:right][:operator].should == '/'
-  #     ast[:right][:right][:right][:right].to_i.should == 2
-  #   end
-
-  #   it "should raise error a incomplet expression like '1 + 5 *" do
-  #     lambda{ parser.parse("1 + 5 *") }.should raise_error(Parslet::ParseFailed)
-  #   end
-
-  # end
 
 end
