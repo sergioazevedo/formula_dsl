c8cba195d19192d486acfb678df210bfc4ceb99d
Sergio Junior <sergior.jr@gmail.com>
2012-03-21 17:11:57 -0300

    first version of grammar ok

--- lib/formula_dsl/formula_parser.rb
+++ lib/formula_dsl/formula_parser.rb
@@ -11,7 +11,9 @@ class FormulaParser < Parslet::Parser
   rule(:space?) { space.maybe }
 
   # Literal's
-  rule(:number) { match('\d').repeat(1) }
+  rule(:number)     { match('\d').repeat(1) }
+  rule(:quote)      { match('"') }
+  rule(:string)     { quote >> space? >> match('.') >> space? >> quote}
   rule(:identifier) { match['\w'].repeat(1) }
 
   # Argument
@@ -49,30 +51,38 @@ class FormulaParser < Parslet::Parser
     (identifier.as(:name) >> lparen >> arglist.as(:args) >> rparen).as(:function)
   end
 
+  rule :string_concat do
+    ( string.as(:left) >> space? >> add_operator >> space? >> ( string ).as(:right) ).as(:concat)
+  end
+
   rule :single_expression do
-    function | addition | subtraction | multiplication | division
+     function | addition | subtraction | multiplication | division | string_concat | number | string
   end
 
   # expression with other expression (compose expression)
   # Ex: expression >> some_op >> number === [ Mont(data) - 1 ]
   rule :subtraction_expression do
-    (single_expression.as(:left) >> space? >> sub_operator >> space? >> number.as(:right) ).as(:-)
+    (single_expression.as(:left) >> space? >> sub_operator >> space? >> (single_expression | number).as(:right) ).as(:-)
   end
 
   rule :addition_expression do
-    (single_expression.as(:left) >> space? >> add_operator >> space? >> number.as(:right) ).as(:+)
+    (single_expression.as(:left) >> space? >> add_operator >> space? >> (single_expression | number).as(:right) ).as(:+)
   end
 
   rule :multiplication_expression do
-    (single_expression.as(:left) >> space? >> mult_operator >> space? >> number.as(:right) ).as(:*)
+    (single_expression.as(:left) >> space? >> mult_operator >> space? >> (single_expression | number).as(:right) ).as(:*)
   end
 
   rule :division_expression do
-    (single_expression.as(:left) >> space? >> div_operator >> space? >> number.as(:right) ).as(:/)
+    (single_expression.as(:left) >> space? >> div_operator >> space? >> (single_expression | number).as(:right) ).as(:/)
+  end
+
+  rule :string_concat_expression do
+    (single_expression.as(:left) >> space? >> add_operator >> space? >> ( expression_list | string ).as(:right) ).as(:concat)
   end
 
   rule :expression_list do
-     addition_expression | subtraction_expression | multiplication_expression | division_expression | single_expression
+    string_concat_expression | addition_expression | subtraction_expression | multiplication_expression | division_expression | single_expression
   end
 
 
--- spec/formula_dsl/formula_parser_spec.rb
+++ spec/formula_dsl/formula_parser_spec.rb
@@ -48,7 +48,33 @@ describe FormulaParser do
       ast = parser.parse('Month(data) / 1')
       ast.to_s.should == %Q({:/=>{:left=>{:function=>{:name=>"Month"@0, :args=>"data"@6}}, :right=>"1"@14}})
     end
+
+    it " should recognize expression with 2 functions 'Month(data) + Year(data)'" do
+      ast = parser.parse('Month(data) + Year(data)')
+      ast.to_s.should == %Q({:concat=>{:left=>{:function=>{:name=>"Month"@0, :args=>"data"@6}}, :right=>{:function=>{:name=>"Year"@14, :args=>"data"@19}}}})
+    end
   end
 
+  context "Expressions with string concatenation" do
+    it " should recognize expression with string concat \"A\" + \"B\"'" do
+      ast = parser.parse('"A" + "B"')
+      ast.to_s.should == %Q({:concat=>{:left=>"\\\"A\\\""@0, :right=>"\\\"B\\\""@6}})
+    end
+
+    it " should recognize expression with string concat '\"A\" + \"B\" + \"C\" ' " do
+      ast = parser.parse('"A" + "B" + "C"')
+      ast.to_s.should == %Q({:concat=>{:left=>{:concat=>{:left=>"\\\"A\\\""@0, :right=>"\\\"B\\\""@6}}, :right=>"\\\"C\\\""@12}})
+    end
+
+    it " should recognize expression with string concat 'Month(data) + \"/\"'" do
+      ast = parser.parse('Month(data) + "/"')
+      ast.to_s.should == %Q({:concat=>{:left=>{:function=>{:name=>"Month"@0, :args=>"data"@6}}, :right=>"\\\"/\\\""@14}})
+    end
+
+    it " should recognize expression with string concat 'Month(data) + \"/\" + Year(data)'" do
+      ast = parser.parse('Month(data) + "/" + Year(data)')
+      ast.to_s.should == %Q({:concat=>{:left=>{:function=>{:name=>"Month"@0, :args=>"data"@6}}, :right=>{:concat=>{:left=>"\\\"/\\\""@14, :right=>{:function=>{:name=>"Year"@20, :args=>"data"@25}}}}}})
+    end
+  end
 
 end
